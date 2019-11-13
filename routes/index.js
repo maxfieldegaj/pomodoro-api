15a0b034829de4b8a787c67e6de35a24d0a763d4
Maxfield Gaj <maxfieldegaj@gmail.com>
2019-11-12 21:33:34 -0600

    init test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const Test = require('../lib/test');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: Test.first(true) });
 });
 
 module.exports = router;
