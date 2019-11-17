02d0b4ed749e581e3a0a8bfadac4d32663fd341f
Maxfield Gaj <maxfieldegaj@gmail.com>
2019-11-17 07:36:51 -0600

    learning to crawl

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: Test.first(true) });
 });
 
-router.get('/test', (req,res,next) => {
-  console.log(driver);
-  driver.searchTextOnGoogle().then(() => {
-    res.status(200);
-  })
+router.get('/test', async (req,res,next) => {
+  try {
+    const {url, action, name} = req.body;
+    const result = await driver.searchTextOnGoogle(url, action, name);
+    if(result.success) {
+      res.status(200).send(result.data);
+    } else {
+      res.status(500).send(result.error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 })
 
 module.exports = router;
--- utils/driver.js
+++ utils/driver.js
@@ -1,21 +1,18 @@
 const webdriver = require('selenium-webdriver');
 
-function searchTextOnGoogle() {
+async function searchTextOnGoogle(url, action, name) {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
 
-    driver.get('https://dknhy7rc6qezw.cloudfront.net/#/').then(() => {
-        driver.findElement(webdriver.By.name('Get Started')).click().then(() => {
-            driver.getTitle().then((title) => {
-                setTimeout(() => {
-                    console.log(title);
-                    driver.quit();
-                }, 5000);
-            })
-        }).catch(err => {
-            console.error(err);
-            driver.quit();
-        })
-    })
+    try {
+        await driver.get(url);
+        const button = await driver[action](webdriver.By.name(name));
+        return {success: true, data: button}
+    } catch(err) {
+        return {success: false, error: err};
+    }
+    finally {
+        // driver.quit();
+    }
 }
 
 module.exports.searchTextOnGoogle = searchTextOnGoogle;
